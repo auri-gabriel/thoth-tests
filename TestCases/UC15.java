bfa999d1b2009c7b316a274a10110e7b523848f8
Guilherme Samuel <guilhermesamuel79@gmail.com>
2019-11-12 05:56:23 +0000

    Update UC15.java

--- TestCases/UC15.java
+++ TestCases/UC15.java
@@ -1,10 +1,10 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
-import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -23,13 +23,12 @@ public class UC15 {
 		// Setando localização do arquivo executável
 		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		driver.get("http://lesse.com.br/tools/thoth-rp/");
     }
     
     @AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		System.out.println("Execução efetuada com sucesso");
+		System.out.println();
     }
     
     @Test
@@ -39,7 +38,8 @@ public class UC15 {
 		driver.findElement(By.name("email")).sendKeys("ketrin_vargas@outlook.com");
 		driver.findElement(By.name("password")).sendKeys("123456");
         driver.findElement(By.className("form-signin")).findElement(By.tagName("BUTTON")).click();
-        for (int i = 0; i < 2; i++) { 
+        String duplicates = "";
+        for (int i = 0; i < 3; i++) { 
             String line = reader.readLine();
             //Seleciona o projeto
             driver.findElement(By.xpath(line)).click();
@@ -50,9 +50,20 @@ public class UC15 {
             //Study Selection
             driver.findElement(By.xpath("/html/body/div/div/div[2]/ul/li[2]/a")).click();
             Thread.sleep(3000);
+            //Check duplicates
+            driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[4]/div[1]/button[6]")).click();
+            Thread.sleep(1000);
+
+            if (line.equals("/html/body/div/div/div[2]/div/div[2]/div/table/tbody/tr[10]/td[3]/a[1]")){
+                duplicates = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[1]")).getText();
+            }
+            //Pressiona "OK"
+            driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/button[1]")).click();
+            Thread.sleep(1000);
             //Retorna para a dashboard
             driver.findElement(By.xpath("/html/body/nav/a")).click();
         }
+        assertEquals("The 1 papers was duplicate", duplicates);
         reader.close();
     }
 }
