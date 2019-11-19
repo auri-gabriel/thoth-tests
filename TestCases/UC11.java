93bcf1d3ad8445bf99bb45475b1d81cc11200607
Guilherme Samuel <guilhermesamuel79@gmail.com>
2019-11-19 19:03:15 +0000

    Update UC11.java

--- TestCases/UC11.java
+++ TestCases/UC11.java
@@ -1,73 +1,91 @@
-package Testes;
+package test;
 
-import static org.junit.Assert.assertEquals;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
+import org.junit.Assert;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class UC11 {
-	
-	static WebDriver driver;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		//setup do selenium				
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        //setup do selenium				
 		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		
-		//ir para a pagina inicial
+		//ir para a p√°gina inicial
 		driver.get("http://lesse.com.br/tools/thoth-rp/");
 		
 		//entrar na conta		
 		driver.findElement(By.linkText("Sign in")).click();				
-		driver.findElement(By.id("InputEmail1")).sendKeys("auri_gabriel@outlook.com");		
-		driver.findElement(By.id("InputPassword")).sendKeys("123456");
+		driver.findElement(By.id("InputEmail1")).sendKeys("guilhermesamuel79@gmail.com");		
+		driver.findElement(By.id("InputPassword")).sendKeys("12345678");
 		driver.findElement(By.cssSelector(".btn-success")).click();
+    }
 
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
 
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    @Test
+    public void solveConflictByQuality() throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader("C:/UC12_parameter.txt"));
+        Path path = Paths.get("C:/UC12_parameter.txt");
+        Long lineCounter = Files.lines(path).count();
+        String message = "";
 
-	@Before
-	public void setUp() throws Exception {
-		
-		
-	}
+        for (int i = 0; i < lineCounter; i++){
+            String line = reader.readLine();
+            //click in a project
+            driver.findElement(By.xpath(line)).click();
+            Thread.sleep(50);
+            //click Conducting
+            driver.findElement(By.xpath("/html/body/div/div/div[1]/a[3]")).click();
+            Thread.sleep(50);
+            //click Study Selection
+            driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/ul/li[1]/a")).click();
+            Thread.sleep(200);
 
-	@After
-	public void tearDown() throws Exception {
-	}
+            if (i + 1 == lineCounter){
+                message = driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[5]/div[3]/div[1]/div")).getText();
+            }
 
-	@Test
-	public void testSelection() throws Exception {
-		
-		driver.get("http://lesse.com.br/tools/thoth-rp/open/84");	//abre a pagina do projeto
-		driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/a[3]")).click();//clica no conducting
-		driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/ul/li[1]/a")).click();//clica no review selection
-		driver.findElement(By.xpath("//*[@id=\"table_conf_paper_selection\"]/tbody/tr[1]")).click();//seleciona o conflito
-		driver.findElement(By.id("status_conflict")).click();//clica no status do conflito
-		driver.findElement(By.xpath("//*[@id=\"status_conflict\"]/option[1]")).click();//clica na opcao do status do conflito
-		Thread.sleep(1000);//espera a tela de successo		
-		assertEquals("Accepted", driver.findElement(By.id("swal2-title")).getText());//confere se o resultado È o esperado
-		driver.findElement(By.cssSelector(".swal2-confirm")).click();//fecha o popup de sucesso
-		driver.close();//termina o teste
-	}
+            else {
+                //click in a conflict
+                driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[9]/div[2]/div/table/tbody/tr[1]/td[2]")).click();
+                Thread.sleep(100);
+                //click in combobox for status
+                driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div[2]/div[1]/div[6]/select")).click();
+                driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div[2]/div[1]/div[6]/select")).sendKeys(Keys.ARROW_UP);
+                driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div[2]/div[1]/div[6]/select")).sendKeys(Keys.ARROW_UP);
+                driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div[2]/div[1]/div[6]/select")).sendKeys(Keys.ENTER);
+                driver.findElement(By.xpath("/html/body/div[3]/div/div[3]/button[1]")).click();
+                Thread.sleep(300);
+                //click on "x"
+                driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div[1]/button/span")).click();
+                Thread.sleep(300);
+            }     
+            //click on dashboard
+            driver.findElement(By.xpath("/html/body/nav/a")).click();      
+        }
 
-
+        Assert.assertEquals(message, "Showing 0 to 0 of 0 entries");
+        reader.close();
+    }
 }
